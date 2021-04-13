338fa6e45baf657c88823960848e74d16cefe946
ygc <937601471@qq.com>
2021-04-13 22:27:35 +0800

    基于JSON对象构造dom

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ var server = http.createServer(function(request, response){
   if(path === '/index.html'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    const string = fs.readFileSync('public/index.html')
+    let string = fs.readFileSync('public/index.html').toString()
+    const page1 = fs.readFileSync('db/page1.json').toString()
+    const array = JSON.parse(page1)
+    const result = array.map(item => `<li>${item.id}</li>`).join('')
+    string = string.replace('{{page1}}', `<ul>${result}</ul>`)
     response.write(string)
     response.end()
   } else if(path === '/main.js'){
